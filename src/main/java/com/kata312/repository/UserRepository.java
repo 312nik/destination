ec24d12d5d1fccc4d42fe81eea53a3eae43513eb
niko-l <111697189+niko-l@users.noreply.github.com>
2022-12-22 14:44:10 +0300

    Update UserRepository.java
    a

--- src/main/java/com/kata312/repository/UserRepository.java
+++ src/main/java/com/kata312/repository/UserRepository.java
@@ -4,5 +4,9 @@ import com.kata312.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+  
+  @EntityGraph(attribuParth="roles")
+  
+  User findUsersByEmail(String email);
 
 }
