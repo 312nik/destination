4f27f1b1b3f30edaf0e7cae244cd9db24f6a302d
niko-l <111697189+niko-l@users.noreply.github.com>
2023-01-30 12:44:13 +0300

    Update editUser.js

--- src/main/resources/static/editUser.js
+++ src/main/resources/static/editUser.js
@@ -8,12 +8,12 @@ async function showEditModal(id) {
     $('#rolesEditUser').empty();
     let user = await getUser(id);
     let editUserForm = $('#formEditUser');
-    editUserForm.find('#id').value=user.id,
-    editUserForm.find('#name').value=user.name,
-    editUserForm.find('#lastName').value=user.lastName,
-    editUserForm.find('#age').value=user.age,  
-    editUserForm.find('#email').value=user.email,
-    editUserForm.find('#password').value=user.password,
+    editUserForm.find('#idEdit').value=user.id,
+    editUserForm.find('#nameEdit').value=user.name,
+    editUserForm.find('#lastnameEdit').value=user.lastName,
+    editUserForm.find('#ageEdit').value=user.age,  
+    editUserForm.find('#emailEdit').value=user.email,
+    editUserForm.find('#passwordEdit').value=user.password,
     
     
     
@@ -22,3 +22,39 @@ async function showEditModal(id) {
     let response = await fetch(url);
     return await response.json();
 }
+    
+ $(async function() {
+    editUser();
+
+});
+function editUser() {
+    
+    editUserForm.addEventListener("submit", event => {
+        event.preventDefault();
+         let selected = Array.from(newUserRoles.options)
+                .filter(option => option.selected)
+                .map(option => option.value.toString());
+
+       
+        }
+
+        fetch("http://localhost:8080/api/users/" + editForm.id.value, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            id:addUserForm.find('#idEdit').val(),
+            name:addUserForm.find('#nameEdit').val().trim(),
+            lastName:addUserForm.find('#lastnameEdit').val().trim(),
+            age:addUserForm.find('#ageEdit').val(),
+            email:addUserForm.find('#emailEdit').val().trim(),
+            password:addUserForm.find('#passwordEdit').val().trim(),
+            roles:selected
+            })
+        }).then(() => {
+            $('#editClose').click();
+            allUsers();
+        })
+    })
+}   
