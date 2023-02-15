cf5c0dfa2e3765e69f4910adf5d1c72959f6bdfb
Moiseev Nikolay <niko-light@bk.ru>
2023-02-15 11:20:04 +0300

    1

--- src/main/java/com/kata312/config/MvcConfig.java
+++ src/main/java/com/kata312/config/MvcConfig.java
@@ -1,7 +1,5 @@
 package com.kata312.config;
 
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
