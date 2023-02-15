7b230d8cecf01d23c7649f458561c039672f7d4d
Moiseev Nikolay <niko-light@bk.ru>
2023-02-15 10:07:04 +0300

    убраны  лишние  импорты

--- src/main/java/com/kata312/config/MvcConfig.java
+++ src/main/java/com/kata312/config/MvcConfig.java
@@ -1,12 +1,9 @@
 package com.kata312.config;
 
 
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
--- src/main/java/com/kata312/config/SecurityConfig.java
+++ src/main/java/com/kata312/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.kata312.config;
 
-import com.kata312.service.UserServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
