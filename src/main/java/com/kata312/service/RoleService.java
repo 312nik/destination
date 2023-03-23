8d21c69c75cf360648b06e5b3fcde4b52698886d
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-23 15:23:10 +0300

    Update RoleService.java

--- src/main/java/com/kata312/service/RoleService.java
+++ src/main/java/com/kata312/service/RoleService.java
@@ -3,7 +3,19 @@ package com.kata312.service;
 import com.kata312.model.Role;
 import java.util.List;
 
-public interface RoleService {
-    List<Role> getAllRole();
-    Role getRoleByName(String roleName);
+public interface DestinationService {
+
+
+
+  Destination  findDestinationById(String id);
+
+  List <Destination> findDestinationByCity(String city);
+
+  List <Destination> findDestinationByCountryName(String countryName);
+
+   void saveDestination(Destination destination);
+
+   void updateDestination(Destination destination);
+
+
 }
