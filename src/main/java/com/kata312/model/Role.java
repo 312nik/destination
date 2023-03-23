7679869855a47f3d36c0bf2120a3bcf12adc0438
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-23 15:19:35 +0300

    Update Role.java

--- src/main/java/com/kata312/model/Role.java
+++ src/main/java/com/kata312/model/Role.java
@@ -1,59 +1,25 @@
-package com.kata312.model;
-
-import org.springframework.security.core.GrantedAuthority;
-import javax.persistence.*;
-import java.util.Objects;
-
-
 @Entity
-@Table(name="roles")
-public class Role implements GrantedAuthority {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column (name="name")
-    private String name;
-    public Role() {
-    }
-    public Role(String name) {
-        this.name = name;
-    }
-    public Role(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    @Override
-    public String toString() {
-        return name;
-    }
-    @Override
-    public String getAuthority() {
-        return getName();
-    }
-    public String getRoleName(Role role){
-        return  role.toString().substring(6);
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role = (Role) o;
-        return id.equals(role.id) && name.equals(role.name);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name="destinations")
+
+public  class Destination {
+
+
+        @Id
+        private String airportCode;
+
+        private String city;
+
+        private String countryCode;
+
+        private String countryName;
+
+        private String airportName;
+
+        private String timezone;
+
+
 }
