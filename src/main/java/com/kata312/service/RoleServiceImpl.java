779b4ff62375c93b49ec232f62526894d1ecb5a2
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-23 15:23:57 +0300

    Update RoleServiceImpl.java

--- src/main/java/com/kata312/service/RoleServiceImpl.java
+++ src/main/java/com/kata312/service/RoleServiceImpl.java
@@ -7,18 +7,39 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class RoleServiceImpl implements RoleService {
-    private  final RoleRepository roleRepository;
+public class DestinationServiceImpl implements DestinationService {
+    private final DestinationRepository destinationRepository;
+
     @Autowired
-    public RoleServiceImpl(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
+    public DestinationServiceImpl(DestinationRepository destinationRepository) {
+        this.destinationRepository = destinationRepository;
     }
+
+
     @Override
-    public List<Role> getAllRole() {
-        return roleRepository.findAll();
+    public Destination findDestinationById(String id) {
+        return destinationRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("=" + id));
     }
+
     @Override
-    public Role getRoleByName(String roleName) {
-        return roleRepository.getRoleByName(roleName);
+    public List<Destination> findDestinationByCity(String city) {
+        return destinationRepository.findByOrderByCity(city);
+    }
+
+    @Override
+    public List<Destination> findDestinationByCountryName(String countryName) {
+        return destinationRepository.findByOrderByCountryName(countryName);
+    }
+
+    @Override
+    @Transactional
+    public void saveDestination(Destination destination) {
+        destinationRepository.save(destination);
+    }
+
+    @Override
+    @Transactional
+    public void updateDestination(Destination destination) {
+        destinationRepository.save(destination);
     }
 }
