b91ec164e644bd39d00804c311bf521ce6009abd
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-23 15:17:44 +0300

    Update CommandLineRunnerImpl.java

--- src/main/java/com/kata312/dbinicialization/CommandLineRunnerImpl.java
+++ src/main/java/com/kata312/dbinicialization/CommandLineRunnerImpl.java
@@ -11,44 +11,40 @@ import java.util.Set;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    public CommandLineRunnerImpl(UserRepository userRepository, RoleRepository roleRepository) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
+
+
+    private final DestinationService destinationService;
+    @Autowired
+    public CommandLineRunnerImpl(DestinationService destinationService) {
+        this.destinationService = destinationService;
     }
+
+
     @Override
     public void run(String... args) throws Exception {
 
-        if(userRepository.findAll().isEmpty()){
+        Destination vnukovo=new Destination();
 
-        roleRepository.save(new Role("ROLE_ADMIN"));
-        roleRepository.save(new Role("ROLE_USER"));
-        Role adminRole = roleRepository.getRoleById(1L);
-        Role userRole = roleRepository.getRoleById(2L);
-        Set<Role> adminRoles = new HashSet<>();
-        adminRoles.add(adminRole);
-        adminRoles.add(userRole);
-        Set <Role> userRoles= new HashSet<>();
-        userRoles.add(userRole);
+        vnukovo.setCity("Moskow");
+        vnukovo.setAirportName("Внуково");
+        vnukovo.setAirportCode("VKO");
+        vnukovo.setTimezone("AA11");
+        vnukovo.setCountryName("Russsia");
+        vnukovo.setCountryCode("RU");
 
-        User admin= new User();
-        admin.setName("Admin");
-        admin.setLastName("Adminov");
-        admin.setEmail("admin@mail.ru");
-        admin.setAge(40);
-        admin.setPassword("$2a$12$zLqrapx439Jx5ckj7ttE3.EbNYu0a.U8S2IoKsf.z63kE4hakQlSu"); //admin
-        admin.setRoles(adminRoles);
-        userRepository.save(admin);
 
-        User user = new User();
-        user.setName("User");
-        user.setLastName("Userov");
-        user.setAge(30);
-        user.setEmail("user@mail.ru");
-        user.setPassword("$2a$12$KfgcyoMND4XibkwHgKmMX.xqTxnfalroJaOl2ewvxVUWK5sqd/tSi"); //user
-        user.setRoles(userRoles);
-        userRepository.save(user);}
+        Destination domodedovd=new Destination();
+
+        domodedovd.setCity("Moskow");
+        domodedovd.setAirportName("Домодедово");
+        domodedovd.setAirportCode("VKO");
+        domodedovd.setTimezone("AA11");
+        domodedovd.setCountryName("Russsia");
+        domodedovd.setCountryCode("RU");
+
+        destinationService.saveDestination(domodedovd);
+        destinationService.saveDestination(vnukovo);
 
     }
 }
+
