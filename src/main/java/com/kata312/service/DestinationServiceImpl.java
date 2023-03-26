41ace79f6a08f8615584719d6a1c67b9228ab838
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-26 16:10:14 +0300

    Update DestinationServiceImpl.java

--- src/main/java/com/kata312/service/DestinationServiceImpl.java
+++ src/main/java/com/kata312/service/DestinationServiceImpl.java
@@ -1,45 +1,53 @@
-package com.kata312.service;
+package ru.uxair.flight.service.impl;
 
-import com.kata312.model.Role;
-import com.kata312.repository.RoleRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.uxair.flight.entity.Destination;
+import ru.uxair.flight.repository.DestinationRepository;
+import ru.uxair.flight.service.DestinationService;
+import ru.uxair.flight.util.exceptions.DestinationNotFoundException;
+
 import java.util.List;
 
 @Service
 public class DestinationServiceImpl implements DestinationService {
-    private final DestinationRepository destinationRepository;
-
+    private DestinationRepository destinationRepository;
     @Autowired
     public DestinationServiceImpl(DestinationRepository destinationRepository) {
         this.destinationRepository = destinationRepository;
     }
 
-
     @Override
     public Destination findDestinationById(String id) {
-        return destinationRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("=" + id));
+        return destinationRepository.findById(id).orElseThrow(DestinationNotFoundException::new);
     }
 
     @Override
     public List<Destination> findDestinationByCity(String city) {
-        return destinationRepository.findByOrderByCity(city);
+        return destinationRepository.findByCity(city);
     }
 
     @Override
     public List<Destination> findDestinationByCountryName(String countryName) {
-        return destinationRepository.findByOrderByCountryName(countryName);
+        return destinationRepository.findByCountryName(countryName);
     }
 
     @Override
-    @Transactional
     public void saveDestination(Destination destination) {
         destinationRepository.save(destination);
+
     }
 
     @Override
-    @Transactional
     public void updateDestination(Destination destination) {
         destinationRepository.save(destination);
+
+    }
+
+    @Override
+    public void deleteDestination(Destination destination) {
+        destinationRepository.delete(destination);
+
     }
 }
