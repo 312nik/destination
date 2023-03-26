8af92a5e46cb6b977fffcbf6fb685d51da7bcc65
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-26 16:07:42 +0300

    Update DestinationRepository.java

--- src/main/java/com/kata312/repository/DestinationRepository.java
+++ src/main/java/com/kata312/repository/DestinationRepository.java
@@ -1,16 +1,15 @@
-package com.kata312.repository;
+package ru.uxair.flight.repository;
 
-import com.kata312.model.User;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import ru.uxair.flight.entity.Destination;
+import java.util.List;
 
 @Repository
-public interface DestinationRepository extends JpaRepository<Destination, String> {
+public interface DestinationRepository extends JpaRepository <Destination,String> {
 
-    List<Destination> findByOrderByCity(String city);
-    List<Destination> findByOrderByCountryName(String countryName);
 
+    List<Destination> findByCity(String city);
+    List <Destination> findByCountryName(String countryName);
 }
 
