e2b309f7c66cf5e9ba9feabfa003639ac3a8a0a3
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-03-27 16:39:11 +0300

    Update CreateDestinationTable.sql

--- src/main/resources/CreateDestinationTable.sql
+++ src/main/resources/CreateDestinationTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Destinations (
-    airport_code varchar(3),primerykey,notnull;
+    airport_code varchar(3),primerykey,notnull, unique;
     city varchar(255),notnull;
     country_code varchar(2),notnull ;
     country_name varchar(255),notnull;
