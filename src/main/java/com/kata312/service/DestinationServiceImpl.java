15c14d8870008bcef24acac2f19d4d008a3dbb9b
MoiseevNikolay <111697189+MoiseevNikolay@users.noreply.github.com>
2023-07-28 16:05:40 +0300

    Update DestinationServiceImpl.java

--- src/main/java/com/kata312/service/DestinationServiceImpl.java
+++ src/main/java/com/kata312/service/DestinationServiceImpl.java
@@ -1,53 +1,74 @@
 package ru.uxair.flight.service.impl;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.uxair.flight.entity.Destination;
 import ru.uxair.flight.repository.DestinationRepository;
 import ru.uxair.flight.service.DestinationService;
-import ru.uxair.flight.util.exceptions.DestinationNotFoundException;
+import ru.uxair.flight.util.exceptions.ResourceNotFoundException;
+
 
 import java.util.List;
 
 @Service
 public class DestinationServiceImpl implements DestinationService {
-    private DestinationRepository destinationRepository;
+    private final DestinationRepository destinationRepository;
     @Autowired
     public DestinationServiceImpl(DestinationRepository destinationRepository) {
         this.destinationRepository = destinationRepository;
     }
 
     @Override
-    public Destination findDestinationById(String id) {
-        return destinationRepository.findById(id).orElseThrow(DestinationNotFoundException::new);
-    }
+    public Destination findDestinationByAirportCode(String airportCode) {
 
+        Destination destination= destinationRepository.findByAirportCodeContainingIgnoreCase(airportCode);
+
+        if (destination != null) {
+            return destination;
+        } else {
+            throw new ResourceNotFoundException(airportCode);
+        }
+
+    }
     @Override
     public List<Destination> findDestinationByCity(String city) {
-        return destinationRepository.findByCity(city);
-    }
+        List <Destination> getByCity=destinationRepository.findByCityContainingIgnoreCase(city,
+                Sort.by("airportName"));
 
+        if ( !getByCity.isEmpty()) {
+            return  getByCity;
+        }else {
+           return null;
+        }
+    }
     @Override
     public List<Destination> findDestinationByCountryName(String countryName) {
-        return destinationRepository.findByCountryName(countryName);
-    }
 
+        List<Destination> getByCountryName=destinationRepository.findByCountryNameContainingIgnoreCase(countryName,
+                Sort.by("city").and(Sort.by("airportName")));
+
+        if ( !getByCountryName.isEmpty()) {
+            return getByCountryName;
+        }else {
+            return null;
+        }
+    }
     @Override
     public void saveDestination(Destination destination) {
         destinationRepository.save(destination);
-
     }
-
     @Override
-    public void updateDestination(Destination destination) {
-        destinationRepository.save(destination);
+    public void updateDestination(Long id ,Destination destination) {
 
+        if (destinationRepository.existsById(String.valueOf(id))) {
+            destination.setId(id);
+            destinationRepository.save(destination);
+        }else {
+            throw new ResourceNotFoundException("=" + id);
+        }
     }
 
-    @Override
-    public void deleteDestination(Destination destination) {
-        destinationRepository.delete(destination);
-
-    }
 }
+
+
